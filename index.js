c0d85c48040b69d5a3ac3cea54f60c6ca455d462
SlyMamba <vainedev@gmail.com>
2020-07-20 12:51:45 -0400

    added echo test 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict"; // Tells the program to ensure that each variable is declared with var,let or const else an error will occur
 
+const request = require("request");
 const express = require("express");
 const bodyParser = require("body-parser");
 const server = express().use(bodyParser.json()); //creates an express http server
@@ -8,6 +9,7 @@ const server = express().use(bodyParser.json()); //creates an express http serve
 // passes the value of port number in the environment variable or port 5000
 const port = process.env.PORT || 5000;
 const MYAPPTOKEN = process.env.CHATTOKEN1;
+
 server.listen(port, ()=>{ console.log("Webhook is listening on port "+ port)});
 
 server.get('/',(request, response)=>{
@@ -18,13 +20,22 @@ server.post("/webhook",(request, response)=>{
     let body = request.body; // post data from the request
 
     //checks if this is an event from a page subscription
+
     if (body.object === 'page'){
         
         //Iterates over the entries there may be multiple if its batched.
         body.entry.forEach(function(entry){
             // Gets the message. entry.messaging is an array but  will only ever contain one message, so we use index[0]
             let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            let sender = webhook_event.sender.id;
+        
+            if(webhook_event.message && webhook_event.message.text){
+                let text = webhook_event.message.text;
+                sendMessage(sender,"Echo: "+ text.substring(0,100));
+                
+            }
+            
+
         });
         // returns '200 OK' status code to all requests
         response.status(200).send('EVENT_RECIEVED');
@@ -36,6 +47,29 @@ server.post("/webhook",(request, response)=>{
 
 
 });
+
+function sendMessage(sender, text){
+    let messageData = {text:text}
+    request({
+        url:"https://graph.facebook.com/2.6/me/messages",
+        qs: {access_token, token},
+        method: "POST",
+        json:{
+            reciept:{id:sender},
+            message:messageData
+        }
+    },(error,response,body)=>{
+        if(error){
+            console.log("Sending message");
+        }else if(response.body.error){
+            console.log(error)
+        }
+
+        }
+
+    });
+
+}
 
 // Adds support for GET requests to our webhook
 
